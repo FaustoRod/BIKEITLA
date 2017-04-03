10757b84d42cc5550b175e4f18a7fe5b53e45165
Roniel Polanco <roniel06.rc@gmail.com>
2017-04-02 20:48:59 -0400

    Estructura principal
    Trabajando el FrontEnd de la estructura principal que va a ser invocada
    por medio de php en cada pagina que se cree...

---
+++ header.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <title></title>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+      <!-- Optional theme -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+      <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <header>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+
+        <div class="navbar-header">
+          <a href="index.php" class="navbar-brand"><strong>Logo</strong></a>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li style="margin:10px;">
+             <button type="button" class="btn btn-default" name="btnReg">Registrate</button>
+          </li>
+
+          <li style="margin:10px;">
+            <button type="button" class="btn btn-default" name="btnIniciar"> Ingresar </button>
+          </li>
+          <li>
+            <form class="navbar-form" role="search">
+            <div class="input-group add-on">
+              <input class="form-control" placeholder="Buscar" name="campoBuscar" id="campoBuscar" type="text">
+              <div class="input-group-btn">
+                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+              </div>
+            </div>
+            </form>
+          </li>
+        </ul> <!--Enlaces de la izquierda-->
+      </div> <!--Contenedor de los elementos-->
+    </div> <!--Contenedor de todo el NavBar-->
+  </nav>  <!--Nav-->
+
+
+  <div class="">  <!--Anuncios(Ads!)-->
+
+  </div>
+
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Inicio</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href=# >Categorias
+          <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Bicicletas</a></li>
+            <li><a href="#">Repuestos</a></li>
+            <li><a href="#">Accesorios</a></li>
+
+          </ul>
+        </li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#" class="#"> Ayuda</a></li>
+        <li><a href="#">Nosotros</a></li>
+      </ul> <!--Enlaces de la izquierda-->
+    </div> <!--Contenedor de los elementos-->
+  </div> <!--Contenedor de todo el NavBar-->
+</nav>  <!--Nav-->
+
+</header>
+
+
+  <div class="contSubCateg">
+
+
+  </div>
+
+
+
+
+
+  <footer>
+
+  </footer>
+</html>
